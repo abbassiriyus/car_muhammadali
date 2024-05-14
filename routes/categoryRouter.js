e143edf07b3c023d3c85da432e89e3ea1d9518b7
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-05-14 15:39:13 +0500

    3
    3

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -5,8 +5,17 @@ const router=require('express').Router()
 // Ma'lumotlarni olish
 router.get('/category', async (req, res) => {
     try {
-      const result = await pool.query('SELECT * FROM category');
-      res.json(result.rows);
+      const category = await pool.query('SELECT * FROM category');
+      const subcategory = await pool.query('SELECT * FROM subcategory');
+for (let i = 0; i < category.rows.length; i++) {
+  category.rows[i].sub=[]
+for (let j = 0; j < subcategory.rows.length; j++) {
+if(category.rows[i].id==subcategory.rows[j].category_id){
+  category.rows[i].sub.push(subcategory.rows[j])
+}
+}
+}
+      res.json(category.rows);
     } catch (error) {
       console.error('Error executing query', error);
       res.status(500).json({ error: 'Internal server error' });
