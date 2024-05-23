abe07a5e23fb5e17604454150c12eda163ac4d2e
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-05-23 16:00:09 +0500

    234
    424

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -76,9 +76,11 @@ router.put('/favorite/:id', async (req, res) => {
 // Delete a favorite
 router.delete('/favorite/:id', async (req, res) => {
   const { id } = req.params;
-
   try {
     const result = await pool.query('DELETE FROM favorite WHERE id = $1 RETURNING *', [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Subcategory not found' });
+    }
     res.json(result.rows[0]);
   } catch (error) {
     console.error('Error executing query', error);
