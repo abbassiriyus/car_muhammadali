5345c7bda9ec58e86a46780a195f15a87a65f0e6
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-06-01 12:30:21 +0500

    123
    321

--- models/database.sql
+++ models/database.sql
@@ -98,8 +98,49 @@ INSERT INTO category (title) VALUES
   ('Mercedes-Benz'), ('Audi'), ('Nissan'), ('Chevrolet'), ('Hyundai');
 
 
+
 INSERT INTO car_image (image,car_id) VALUES
-  ('https://hips.hearstapps.com/hmg-prod/images/db2023au01469-65b171f5d572f.jpg',3),('https://www.autonocion.com/wp-content/uploads/2019/11/vw-golf111.jpg',3),('https://avatars.mds.yandex.net/get-vertis-journal/4212087/DB2023AU00395_medium.jpg_1685605383322/orig',3),('https://media.ed.edmunds-media.com/bmw/3-series/2023/oem/2023_bmw_3-series_sedan_330i-xdrive_fq_oem_1_600.jpg',4),('https://www.bmwusa.com/content/dam/bmw/common/vehicles/2023/my24/3-series/sedan/overview/mobile/BMW-MY24-3Series-Overview-Design-01-Mobile.jpg',4),('https://imgd.aeplcdn.com/1056x594/n/ksvkl3a_1579019.jpg?q=80',5),('https://www.topgear.com/sites/default/files/2021/11/Mercedes_C300D_0003.jpg',5),('https://www.mbusa.com/content/dam/mb-nafta/us/myco/my24/c-class/gallery/series/gallery-class/2024-C-SEDAN-GAL-010-R-WP.jpg',5),('https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Audi_A4_B9_sedans_%28FL%29_1X7A2441.jpg/1200px-Audi_A4_B9_sedans_%28FL%29_1X7A2441.jpg',6),('https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Audi_A4_Avant_B9_IMG_1973.jpg/800px-Audi_A4_Avant_B9_IMG_1973.jpg',6),('https://media.ed.edmunds-media.com/nissan/altima/2023/oem/2023_nissan_altima_sedan_vc-t-sr_fq_oem_1_1280.jpg',7),('https://avatars.mds.yandex.net/get-verba/997355/2a0000016eb273ff6dd826c5442bfb75440e/cattouchret',7),('https://upload.wikimedia.org/wikipedia/commons/d/d0/2020_Chevrolet_Silverado_1500_High_Country%2C_front_10.25.20.jpg',8),('https://avatars.mds.yandex.net/get-verba/1540742/2a000001830d187b363750233b43dd670b82/cattouchretcr',8),('https://s7d1.scene7.com/is/image/hyundai/2024-sonata-hev-dn8hev-0458-carousel:2560-2560x1240?qlt=85,0&fmt=webp',9),('https://avtoremont.uz/d/novaya-hyundai-sonata-2021.jpg',9),('https://topruscar.ru/assets/images/kt/kt2021_hyundai-sonata_001.jpg',9),('https://motor.ru/imgs/2023/03/27/07/5850601/b7834cb797d709635d14892ca09f97f395ea4137.jpg',10),('https://hips.hearstapps.com/hmg-prod/images/2024-toyota-camry-102-64cbc4858e198.jpg?crop=0.469xw:0.527xh;0.332xw,0.190xh&resize=768:*',10);
+  ('https://www.automoblog.com/wp-content/uploads/2020/02/01-2021-Honda-Civic-Type-R-Limited-Edition.jpg',1),
+  ('https://cdn.motor1.com/images/mgl/B7eBA/s1/2021-honda-civic-type-r-limited-edition-exterior-front-quarter.jpg',1),
+  ('https://www.hondacarmine.ru/images/text/729.jpg',1),
+  ('https://images.cars.com/cldstatic/wp-content/uploads/510581268-1425511299511.jpeg',2),
+  ('https://img.supercarros.com/AdsPhotos/1024x768/0/11504103.jpg?wmo=c86d9d83ec8d5d3d5e3e0bb3e504071a0a9456cf646fa39bcdd8825046d24ada8dd583e665b099ad649f026515bb54a205774d71c0a06062c10d74ee018987e0',2),
+  ('https://www.allcarz.ru/wp-content/uploads/2016/11/foto-mustang-6-fl_12.jpg',2),
+  ('https://autopremiumgroup.ru/m/_versions/info/ford/mustang/2020/18_preview_image.jpg',2),
+  ('https://images.prismic.io/carwow/2b4b884f-fa2b-40e2-9182-2d2c9450ac5b_37018-ThenewVolkswagenGolfeHybrid.jpg?auto=format&cs=tinysrgb&fit=crop&q=60&w=750',3),
+  ('https://repost.uz/storage/uploads/979-1696967394-avto11-post-material.jpeg',3),
+  ('https://mediacloud.carbuyer.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1605613521/carbuyer/2020/11/Volkswagen%20Golf%20GTI%20hatchback%20review%202020%20carbuyer%20-14.jpg',3),
+  ('https://www.auto-mgn.ru/resources/images/upload/00052352374e94938257_990x0_C.jpg',3),
+  ('https://s.auto.drom.ru/i24203/c/photos/fullsize/volkswagen/golf/volkswagen_golf_652589.jpg',3),
+  ('https://img-c.drive.ru/models.photos/0000/000/000/001/451/48d6600d18e9571a-large.jpg',4),
+  ('https://www.bmw.uz/content/dam/bmw/common/all-models/3-series/sedan/2018/navigation/bmw-3-series-modellfinder.png',4),
+  ('https://m.atcdn.co.uk/vms/media/w612/f80f238753dd41af898e6f87f94ec855.jpg',4),
+  ('https://avatars.dzeninfra.ru/get-zen_doc/198554/pub_5bc0b09f38a33e00a9f7c4ff_5bc0b0a4cf1f9400abd50a10/scale_1200',4),
+  ('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUP4F5Wqr7wq9-wy5jBYpmN5Qi_nwdo940FQ&s',5),
+  ('https://mercedes-tashkent.uz/sites/default/files/video/c-class-sedan.jpg',5),
+  ('https://s0.rbk.ru/v6_top_pics/media/img/8/94/754788638961948.jpeg',5),
+  ('https://img.mercedes-benz-kiev.com/data/lineup/c-class-limousine/c-class-limousine-5.jpeg',5),
+  ('https://www.audicentrebrighton.com.au/content/dam/iph/international/au/general_assets/Models/A4/A4_Sedan/Stage/1920x600-A4-sedan-mirror-hero-image.png/_jcr_content/renditions/cq5dam.thumbnail.640.360.iph.hero.png?imwidth=320&imdensity=1',6),
+  ('https://carexpert.ru/img/int1200/audi4i19-04.jpg',6),
+  ('https://carexpert.ru/img/int1200/audi4i19-05.jpg',6),
+  ('https://media.ed.edmunds-media.com/for-sale/cb-1n4al3ap2gc289972/img-1-600x263.jpg',7),
+  ('https://cimg3.ibsrv.net/ibimg/hgm/2000x1125-1/100/552/2016-nissan-altima-2-5-sl_100552023.jpg',7),
+  ('https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i2314279/2016_nissan_altima_dashboard.jpg',7),
+  ('https://cdn.jdpower.com/JDPA_2020%20Chevrolet%20Silverado%201500%20LT%20Trail%20Boss%20Black%20Front%20View%20Small.jpg',8),
+  ('https://www.chevrolet.com/content/dam/chevrolet/na/us/english/index/vehicles/2024/trucks/silverado-ld-1500/mov/2024-silverado-specialeditions-03.jpg?imwidth=960',8),
+  ('https://specialtyvehicleengineering.com/wp-content/uploads/2022/07/2023_silverado_small_1.jpg',8),
+  ('https://www.allcarz.ru/wp-content/uploads/2017/12/foto-silverado-4_12.jpg',8),
+  ('https://autopremiumgroup.ru/m/_versions/info/chevrolet/silverado_2500_hd/2021/features/6_banner.jpg',8),
+  ('https://autopremiumgroup.ru/m/_versions/info/chevrolet/silverado_1500/2020/13_preview_image.jpg',8),
+  ('https://hips.hearstapps.com/hmg-prod/images/2024-hyundai-sonata-n-line-101-64ef8c4d94261.jpg?crop=0.864xw:0.791xh;0.0765xw,0.0791xh&resize=980:*',9),
+  ('https://i.gaw.to/vehicles/photos/40/34/403449-2024-hyundai-sonata.jpg',9),
+  ('https://s7d1.scene7.com/is/image/hyundai/2024-sonata-hev-dn8hev-0458-carousel:2560-2560x1240?qlt=85,0&fmt=webp',9),
+  ('https://lh3.googleusercontent.com/proxy/SVVDMrNAsfYzWMhMi32aD8DWDdNn-HKnMMKjkEtxeO5HV9jMe71F06hE7ADg9cakYRydIekwm1Ama7KmIesSlNBSkQ',9),
+  ('https://sonata-club.net/photo/files/1/hyundai-sonata-2020-us-spec-18.jpg',9),
+  ('https://s.auto.drom.ru/d24289/foreignimport/737650502/803294692.jpg',9),
+  ('https://toyotacamry.ru/gallery/files/1/2018-toyota-camry-xle-by-zeid-nasser-1-1600x915.jpg',10),
+  ('https://www.auto-loker.ru/upload/iblock/7c3/salon2_camry70_13.jpg',10),
+  ('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyxegz3K1tOMcikyjjhZnGg3FnM2YAsA8qMw&s',10);
 
 
 INSERT INTO subcategory (category_id, title) VALUES
@@ -141,16 +182,16 @@ INSERT INTO car (
   engine_condition, location, description
 ) VALUES
   (
-    'Toyota Camry', 'https://cdn-images.motor.es/image/m/1320w.webp/fotos-noticias/2020/07/toyota-camry-2021-gama-usa-specs-oficial-202069160-1594812986_1.jpg', 25, 'ABC123', 25000, 2020, 'Toyota', 'Camry',
+    'Toyota Camry', 'https://tengrimotors.kz/photo/%D0%9A%D0%B0%D0%BC%D1%80%D0%B8%2070%20%D1%87%D0%B5%D1%80%D0%BD%D1%8B%D0%B9/IMG_0630.JPG', 25, 'ABC123', 25000, 2020, 'Toyota', 'Camry',
     'Black', 'Silver', 'Automatic', 50000, 'New York',1,1,true, true, true,'Excellent', 'New York, NY', 'This is a high-quality Toyota Camry with low mileage.'
   ),
   (
-    'Honda Civic', 'https://i.pinimg.com/736x/a4/a5/4d/a4a54d8f5b1c09f597253982c076bf42.jpg', 30, 'DEF456', 22000, 2018, 'Honda', 'Civic',
+    'Honda Civic', 'https://www.topgear.com/sites/default/files/images/cars-road-test/2020/09/fb96baaf2acc8929744af2d1a9d7b7e8/308638_2020_civic_type_r_limited_edition.jpg', 30, 'DEF456', 22000, 2018, 'Honda', 'Civic',
     'Beige', 'Gray', 'Manual', 70000, 'California',2,3,
     true, true, true, 'Good', 'Los Angeles, CA', 'This Honda Civic is in great condition with a manual transmission.'
   ),
   (
-    'Ford Mustang', 'https://cdn.motor1.com/images/mgl/J7EjQ/s1/electric-ford-mustang-by-charge-cars.jpg', 40, 'GHI789', 35000, 2021, 'Ford', 'Mustang',
+    'Ford Mustang', 'https://upload.wikimedia.org/wikipedia/commons/d/d1/2018_Ford_Mustang_GT_5.0.jpg', 40, 'GHI789', 35000, 2021, 'Ford', 'Mustang',
     'Red', 'Black', 'Automatic', 20000, 'Florida',
     3,5,
     true, true, true, 'Excellent', 'Miami, FL', 'This Ford Mustang is a powerful and stylish sports car.'
@@ -178,7 +219,7 @@ INSERT INTO car (
     true, true, true, 'Good', 'Seattle, WA', 'This Audi A4 is a well-equipped luxury sedan with a smooth ride.'
   ),
   (
-    'Nissan Altima', 'https://hips.hearstapps.com/hmg-prod/images/2019-nissan-altima-12-source-1200x699-1562955653.jpg?crop=0.760xw:0.798xh;0.218xw,0&resize=980:*', 27, 'VWX234', 20000, 2015, 'Nissan', 'Altima',
+    'Nissan Altima', 'https://media.ed.edmunds-media.com/for-sale/cb-1n4al3ap2gc289972/img-1-600x263.jpg', 27, 'VWX234', 20000, 2015, 'Nissan', 'Altima',
     'Beige', 'White', 'Automatic', 80000, 'Ohio',
    8,15,
     true, true, true, 'Good', 'Columbus, OH', 'This Nissan Altima is a reliable and affordable midsize sedan.'
